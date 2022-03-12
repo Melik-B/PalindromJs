c6b9330fa76bed43357fc58fb1f7b3d38ebda663
Melik-B <melikbaykal06@gmail.com>
2022-03-12 14:36:43 +0300

    Add project files.

---
+++ PalindromJs/wwwroot/js/Palindrom.js
@@ -0,0 +1,8 @@
+﻿//function tersCevir(var kelime) {
+//    var TersKelime = "";
+//    for (var i = kelime.Length - 1; i >= 0; i--)
+//    {
+//        TersKelime += kelime[i];
+//    }
+//    return TersKelime;
+//}
